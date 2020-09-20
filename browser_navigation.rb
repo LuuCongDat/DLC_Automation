8c30147bdb7191f039cf0e68127f923a4fd9e8ca
LuuCongDat <loivedemdong595@gmail.com>
2020-09-20 15:41:48 +0700

    Revert "add file IDE"
    This reverts commit e6a53deb0dd38804935b6f37c3b737f8f73bfa82.

--- browser_navigation.rb
+++
@@ -1,9 +0,0 @@
-require 'selenium-webdriver'
-require 'webdrivers'
-driver = Selenium::WebDriver.for :chrome
-driver.get 'https://www.google.com.vn/'
-search_bar = driver.find_element(name: 'q')
-sleep 2
-search_bar.send_keys 'ITMS Coaching'
-sleep 2
-driver.quit
--- test.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'selenium-webdriver'
-require 'webdrivers'
- 
-browser = Selenium::WebDriver.for :chrome
-html_file = "C:/Users/LCD/Desktop/Automation/test.html"
-browser.get "file:///" + html_file 
-sleep 5
-browser.quit
