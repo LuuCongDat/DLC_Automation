7ad46427debf0d27c58f6d2473e578873e8221d0
LuuCongDat <loivedemdong595@gmail.com>
2020-09-23 20:33:58 +0700

    Homework

---
+++ case2.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'selenium-webdriver'
+require 'webdrivers'
+
+driver = Selenium::WebDriver.for :chrome
+driver.get 'https://www.google.com/'
+driver.manage.window.maximize
+driver.manage.timeouts.implicit_wait = 3
+driver.quit
